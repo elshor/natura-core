61374de33290064fff3c9cd10e492bd69168b32c
Elchanan Shor <eshor@arza.biz>
2021-01-03 14:31:05 +0200

    DEV add assumption checks

--- reference.js
+++ reference.js
@@ -1,4 +1,8 @@
+import { assume } from "./error"
+
 export default function reference(label,valueType,path){
+	assume(label,'label of reference must be specified');
+	assume(valueType,'value type for reference must be specified');
 	return {
 		$type:'reference',
 		label,
