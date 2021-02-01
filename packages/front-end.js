cf0118cc1dcfe5306f6be28c2d5bd75063ddfe00
Elchanan Shor <eshor@arza.biz>
2021-02-01 22:26:50 +0200

    REF move definitions to jquery-ui

--- packages/front-end.js
+++ packages/front-end.js
@@ -1,69 +1,12 @@
 const entities = [
 	{
-		name: 'alert',
-		title:'alert message',
-		isa:['action'],
-		pattern:'alert <<a string>>',
-		fn:'alert@natura/lib/front-end(a string)'
-	},
-	{
-		name:'element by id',
-		pattern:'element with id <<id>>',
-		isa:['expression'],
-		fn:'elementWithId@natura/lib/front-end(id)',
-		valueType:'an element'
-	},
-	{
-		name:'element matching selector',
-		pattern:'element matching <<selector>>',
-		isa:['expression'],
-		properties:{
-			selector:{type:'a string',placeholder:'selector to match'}
-		},
-		fn:'elementMatching@natura/lib/front-end(selector)',
-		valueType:'an element'
-	},
-	{
 		name:'front end script',
 		show:['interactions'],
 		properties:{
 			interactions:{type:'event handler*',expanded:true}
 		}
 	},
-	{
-		name:'change style',
-		title:'change style of element',
-		pattern:'change <<name>> of <<an element>> to <<value>>',
-		fn:'changeStyle@natura/lib/front-end(an element,name,value)',
-		isa:['action'],
-		properties:{
-			value:{
-				type:'text',
-				placeholder:'value of style'
-			},
-			name:{
-				type:'style name',
-				placeholder:'style property'
-			}
-		}
-	}
 ]
-
-registerStyle('background color','backgroundColor');
-registerStyle('font color','color');
-registerStyle('border style','borderStyle');
-registerStyle('border width','borderWidth');
-registerStyle('margin','margin');
-registerStyle('font size','fontSize');
-registerStyle('border color','borderColor');
-registerStyle('font family','fontFamily');
-registerStyle('font weight','fontWeight');
-
-
-
-
-
-
 
 export default {
 	name:'data store',
@@ -72,16 +15,4 @@ export default {
 		members:entities,
 		model:{isa:[]}
 	}
-}
-
-function registerStyle(name,cssName,valueType='text'){
-	cssName = cssName || name.split(' ').join('-')
-	entities.push({
-		name:name + '.style',
-		title:name,
-		fn:{
-			value:cssName
-		},
-		isa:['style name']
-	});
 }
