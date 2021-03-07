eb9f3d74007d8f330fbd9bf32b906a72d5991bb3
Elchanan Shor <eshor@arza.biz>
2021-03-07 10:42:08 +0200

    DEM wix demo

--- packages/front-end.js
+++ packages/front-end.js
@@ -6,6 +6,36 @@ const entities = [
 			interactions:{type:'event handler*',expanded:true}
 		}
 	},
+	{
+		name:'wix script',
+		show:['content','interactions'],
+		properties:{
+			content:{title:'content connections',type:'content connection*',expanded:true},
+			interactions:{type:'event handler*',expanded:true}
+		}
+	},
+	{
+		name:'content connection',
+		pattern:'connect <<data>> to <<element>>',
+		show:['n','mode','steps'],
+		properties:{
+			data:{
+				placeholder:'select collection to connect',
+				title:'number of items',
+				type:'string'
+			},
+			element:{
+				placeholder:'select repeater to connect',type:'string'
+			},
+			n:{type:'number',placeholder:'number of items to display',title:'number of items'},
+			mode:{options:['read only','read-write'],type:'string',init:'read only'},
+			steps:{type:'processing step*',placeholder:'define processing step such as filter or sort',expanded:true}
+		}
+	},
+	{
+		name:'processing step',
+		placeholder:'processing step such as filter or sort'
+	}
 ]
 
 export default {
