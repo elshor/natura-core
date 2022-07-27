08adf419dc2789de34f884808bd151d5524fcdc8
Elchanan Shor <eshor@arza.biz>
2022-07-27 18:56:37 +0300

    REF moved functions to parent class

--- base-type.js
+++ base-type.js
@@ -3,40 +3,10 @@
  *   All rights reserved.
  */
 import SuperType from './super-type.js'
-import Type from './type.js'
 
 export default class BaseType extends SuperType{
 	constructor(type,dictionary){
 		super('base type',dictionary)
 		this.type = type || 'any';
 	}
-	toString(){
-		return this.typeString;
-	}
-
-	get typeString(){
-		return this.type.typeString || this.type.toString();
-	}
-	/** A search string is always the singular of a type */
-	get searchString(){
-		return this.isCollection? this.singular.typeString : this.typeString;
-	}
-
-	get isCollection(){
-		const str = this.typeString;
-		return str? str.match(/\*$/) !== null : false;
-	}
-
-	get isArray(){
-		return this.isCollection;
-	}
-
-	get singular(){
-		if(this.isCollection){
-			return Type(this.typeString.substr(0,this.typeString.length-1),null,this.dictionary);
-		}else{
-			return this;
-		}
-	}
-
 }
--- super-type.js
+++ super-type.js
@@ -5,15 +5,46 @@
 /**
  * Used as base to extend other types
  */
+import Type from "./type.js";
 export default class SuperType{
 	constructor(type,dictionary){
 		this.dictionary = dictionary;
 		this.$type = type;
 	}
+	toString(){
+		return this.typeString;
+	}
+
+	get typeString(){
+		return this.type.typeString || this.type.toString();
+	}
+	/** A search string is always the singular of a type */
+	get searchString(){
+		return this.isCollection? this.singular.typeString : this.typeString;
+	}
+
+	get isCollection(){
+		const str = this.typeString;
+		return str? str.match(/\*$/) !== null : false;
+	}
+
+	get isArray(){
+		return this.isCollection;
+	}
+
+	get singular(){
+		if(this.isCollection){
+			return Type(this.typeString.substr(0,this.typeString.length-1),null,this.dictionary);
+		}else{
+			return this;
+		}
+	}
+
 	get isTypeObject(){
 		return true;
 	}
 	get spec(){
+		console.log('get spec',dictionary,type);
 		return this.dictionary? this.dictionary.getTypeSpec(this) : {}
 	}
 
