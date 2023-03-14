572d7da0035cb9fab5ae2b71e67120b6e338fc74
Elchanan Shor <elshor@gmail.com>
2023-03-14 12:04:06 +0200

    DEV some enhacements

--- tools/parse-text.js
+++ tools/parse-text.js
@@ -1,12 +1,15 @@
 import { getDictionary } from "../dictionary.js";
-const packages = ["interact@dev", "ga@dev", "date-time@dev","core@dev"];
-const text = 'show a table of events named "page_view" display';
+const packages = ["interact@dev", "ga@dev", "date-time@dev","core@dev","define@dev"];
+const text = 'define number of events named "page_view" where web page ';
+//const text = 'define number of events named "page_view" where web page is the web page';
+const target = 'trait<web page>'
 
 async function parse(text, packages){
 	const dictionary = await getDictionary(packages);
-	const parsed = dictionary.parse(text);
+	const parsed = dictionary.parse(text, target);
 	if(parsed.length === 0){
 		console.log('No parsed found');
+		process.exit(-1)
 	}else{
 		console.log(JSON.stringify(parsed[0],null, '  '));
 	}
