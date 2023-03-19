6a8c1d8ce198c19c54715d826202cbe430d6504d
Elchanan Shor <elshor@gmail.com>
2023-03-19 08:48:20 +0200

    DEV some enhancements

--- tools/parse-text.js
+++ tools/parse-text.js
@@ -1,17 +1,26 @@
 import { getDictionary } from "../dictionary.js";
 const packages = ["interact@dev", "ga@dev", "date-time@dev","core@dev","define@dev"];
-const text = 'define number of events named "page_view" where web page ';
-//const text = 'define number of events named "page_view" where web page is the web page';
-const target = 'trait<web page>'
+const text = 'define number of sessions where exit page is the web page';
+const target = 'type:interact action'
 
 async function parse(text, packages){
 	const dictionary = await getDictionary(packages);
 	const parsed = dictionary.parse(text, target);
 	if(parsed.length === 0){
-		console.log('No parsed found');
+		const wants = dictionary.parseWants(text, target);
+		console.info('No parse found');
+		const tokens = dictionary.suggestTokens(text);
+		console.info('suggestedTokens:',tokens.length > 0?tokens.info(',') : 'no suggestions')
+		console.info(
+			'WANTS',
+			Object.entries(wants.wants).map(([key,value])=>{
+				return key + ':\n  ' + value.map(item=>item.toString()).join('\n  ')
+			}).join('\n')
+		)
+	
 		process.exit(-1)
 	}else{
-		console.log(JSON.stringify(parsed[0],null, '  '));
+		console.info(JSON.stringify(parsed[0],null, '  '));
 	}
 }
 await parse(text, packages);
